edc5d2f8d9848ad9669664900a32ec1b4b7079f1
mikkel <mikkel.andersen2810@gmail.com>
2024-03-14 09:22:02 +0100

    OPgave 2

--- L04 - Hashing/src/hashing/HashSetLinearProbing.java
+++ L04 - Hashing/src/hashing/HashSetLinearProbing.java
@@ -4,82 +4,99 @@ package hashing;
  * This class implements a hash set using separate chaining.
  */
 public class HashSetLinearProbing {
-	private Object[] buckets;
-	private int currentSize;
-	private static final String DELETED = "DELETED";
+    private Object[] buckets;
+    private int currentSize;
+    private static final String DELETED = "DELETED";
 
-	/**
-	 * Constructs a hash table.
-	 *
-	 * @param bucketsLength
-	 *            the length of the buckets array
-	 */
-	public HashSetLinearProbing(int bucketsLength) {
-		buckets = new Object[bucketsLength];
-		currentSize = 0;
-	}
+    /**
+     * Constructs a hash table.
+     *
+     * @param bucketsLength the length of the buckets array
+     */
+    public HashSetLinearProbing(int bucketsLength) {
+        buckets = new Object[bucketsLength];
+        currentSize = 0;
+    }
 
-	/**
-	 * Tests for set membership.
-	 *
-	 * @param x
-	 *            an object
-	 * @return true if x is an element of this set
-	 */
-	public boolean contains(Object x) {
+    /**
+     * Adds an element to this set.
+     *
+     * @param x an object
+     * @return true if x is a new object, false if x was already in the set
+     */
+    public boolean add(Object x) {
+        int h = hashValue(x);
+        while (buckets[h] != null) {
+            if (buckets[h].equals(x)) {
+                return false; // already in the set
+            }
+            h = (h + 1) % buckets.length;
+        }
+        buckets[h] = x;
+        currentSize++;
+        return true;
+    }
 
-		// TODO
-		return false;
-	}
+    /**
+     * Tests for set membership.
+     *
+     * @param x an object
+     * @return true if x is an element of this set
+     */
+    public boolean contains(Object x) {
+        int h = hashValue(x);
+        while (buckets[h] != null) {
+            if (buckets[h].equals(x)) {
+                return true;
+            }
+            h = (h + 1) % buckets.length;
+        }
+        return false;
+    }
 
-	/**
-	 * Adds an element to this set.
-	 *
-	 * @param x
-	 *            an object
-	 * @return true if x is a new object, false if x was already in the set
-	 */
-	public boolean add(Object x) {
+    /**
+     * Removes an object from this set.
+     *
+     * @param x an object
+     * @return true if x was removed from this set, false if x was not an
+     * element of this set
+     */
+    public boolean remove(Object x) {
+        int h = hashValue(x);
+        while (buckets[h] != null) {
+            if (buckets[h].equals(x)) {
+                buckets[h] = DELETED;
+                currentSize--;
+                return true;
+            }
+            h = (h + 1) % buckets.length;
+        }
+        return false;
+    }
 
-		return false;
-	}
+    /**
+     * Gets the number of elements in this set.
+     *
+     * @return the number of elements
+     */
+    public int size() {
+        return currentSize;
+    }
 
-	/**
-	 * Removes an object from this set.
-	 *
-	 * @param x
-	 *            an object
-	 * @return true if x was removed from this set, false if x was not an
-	 *         element of this set
-	 */
-	public boolean remove(Object x) {
-		// TODO
-		return false;
-	}
+    private int hashValue(Object x) {
+        int h = x.hashCode();
+        if (h < 0) {
+            h = -h;
+        }
+        h = h % buckets.length;
+        return h;
+    }
 
-	/**
-	 * Gets the number of elements in this set.
-	 *
-	 * @return the number of elements
-	 */
-	public int size() {
-		return currentSize;
-	}
-
-	private int hashValue(Object x) {
-		int h = x.hashCode();
-		if (h < 0) {
-			h = -h;
-		}
-		h = h % buckets.length;
-		return h;
-	}
-
-	// method only for test purpose
-	public void writeOut() {
-		for (int i = 0; i < buckets.length; i++) {
-			System.out.println(i + "\t" + buckets[i]);
-		}
-	}
+    // method only for test purpose
+    public void writeOut() {
+        for (int i = 0; i < buckets.length; i++) {
+            System.out.println(i + "\t" + buckets[i]);
+        }
+    }
 
 }
